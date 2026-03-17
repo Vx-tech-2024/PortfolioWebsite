3f62f2aa5389fa8c4de2014bff0fc1901860a944
Vx-tech <xaviervianney8@gmail.com>
2026-03-17 16:08:43 +0300

    Setting up the vite project using react framework and ensuring tailwind is working

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">TTailwind is working</h1>
+    </div>
+  )
+}
+
+export default App
